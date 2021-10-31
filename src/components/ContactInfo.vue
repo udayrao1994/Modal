65c03ee51fa4b53c1c59135ccfef83f461526a39
udayrao1994 <udaysingh1994@gmail.com>
2021-10-31 23:55:26 +0530

    submit

--- src/components/ContactInfo.vue
+++ src/components/ContactInfo.vue
@@ -1,47 +1,53 @@
 <template>
   <div id="pagesize">
-   
     <div class="row">
-      <div class=" col-sm-12 col-lg-6 col-md-6 col-xs-12 " >
-      <label  class="label label-default">First Name<i class="asterisk">*</i> </label>
-      <Input @handleInput="getValue1" name="fname" />
-      
+      <div class="col-sm-12 col-lg-6 col-md-6 col-xs-12">
+        <label class="label label-default"
+          >First Name<i class="asterisk">*</i>
+        </label>
+        <Input @handleInput="getValue1" name="fname" error="Invalid first name"/>
       </div>
-      
-      <div class= "col-sm-12 col-lg-6 col-md-6 col-xs-12 " >
-      <label class="label label-default">Last Name<i class="asterisk">*</i> </label>
-      <Input @handleInput="getValue1" name="lname" />
+
+      <div class="col-sm-12 col-lg-6 col-md-6 col-xs-12">
+        <label class="label label-default"
+          >Last Name<i class="asterisk">*</i>
+        </label>
+        <Input @handleInput="getValue1" name="lname" error="Invalid last name"/>
       </div>
     </div>
-    
+
     <div class="row">
       <div class="col-sm-12 col-lg-6 col-md-6 col-xs-12">
-       <label class="label label-default">Email<i class="asterisk">*</i> </label>
-        <Input @handleInput="getValue1" name="email" />
+        <label class="label label-default"
+          >Email<i class="asterisk">*</i>
+        </label>
+        <Input @handleInput="getValue1" name="email" error="Enter valid email" />
       </div>
       <div class="col-sm-12 col-lg-6 col-md-6 col-xs-12">
-       <label class="label label-default">Phone<i class="asterisk">*</i> </label>
-        <Input @handleInput="getValue1" name="mobile" />
+        <label class="label label-default"
+          >Phone<i class="asterisk">*</i>
+        </label>
+        <Input @handleInput="getValue1" name="mobile" error="Enter valid mobile number" />
       </div>
     </div>
 
     <br />
     <div class="row">
-      <div class="col-8 col-sm col-lg col-md ">
-      
-        <span class="mr-2 " id="labletext">My Offer </span>
-        <span class="label label-default mr-2 " id="labletext2">
+      <div class="col-8 col-sm col-lg col-md">
+        <span class="mr-2" id="labletext">My Offer </span>
+        <span class="label label-default mr-2" id="labletext2">
           Current Asking Price: $9,999</span
         >
-         </div>
-          </div>
-          <div class="row">
-         <label id="input3" class="label label-default mt-2">Enter Offer<i class="asterisk">*</i> </label>
-      <div id="input2" class="col-sm-12 col-lg-6 col-md-6 col-xs-12">
-      <Input @handleInput="getValue1" name="myOffer" />
       </div>
-     </div>
-   
+    </div>
+    <div class="row">
+      <label id="input3" class="label label-default mt-2"
+        >Enter Offer<i class="asterisk">*</i>
+      </label>
+      <div id="input2" class="col-sm-12 col-lg-6 col-md-6 col-xs-12">
+        <Input @handleInput="getValue1" name="myOffer" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -49,58 +55,49 @@
 <script>
 import Input from "./Input.vue";
 
-
 export default {
   components: {
-  
-  Input,
-  
-    
+    Input,
   },
   data: () => {
-        return {
-            item: {},
-        }
-    },
+    return {
+      item: {},
+    };
+  },
   methods: {
-    getValue1(value,name){
-      this.$emit('handleInputs',value,name)
+    getValue1(value, name) {
+      this.$emit("handleInputs", value, name);
     },
-  }
-  }
-
+  },
+};
 </script>
 
 <style scoped>
-
-#input3{
-
+#input3 {
   padding-left: 15px;
 }
 
-#pagesize{
+#pagesize {
   padding-right: 10px;
-    padding-left: 10px;
+  padding-left: 10px;
 }
 #labletext {
   color: black;
   font-weight: bold;
   padding: 4px;
-padding-right: 10px;
-   
+  padding-right: 10px;
+
   font-family: Verdana, Arial, Helvetica, sans-serif;
   font-size: xx-medium;
 }
 
 #labletext2 {
   font-style: italic;
-  
 }
-.asterisk{
+.asterisk {
   color: red;
 }
-.label{
-  padding-top:5px
+.label {
+  padding-top: 5px;
 }
-
 </style>
--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,50 +1,72 @@
 
 <template>
-  <div class="form-outline"> 
-    <input type="text" id="validationDefault02"  v-model="value"  required @change="e=>handleField(e)" :name="`${name}` ">
-    <!-- <div class="mt-2">Value: {{ text }}</div> -->
+  <div class="form-outline" :class="{invalid: usernameValidity ==='invalid'}">
+    <input
+      type="text"
+      class="inputtext"
+      v-model.trim="value"
+      required
+      @change="(e) => handleField(e)"
+      :name="`${name}`"
+      @blur="validateInput"
+    />
+      <div  class="inputtexterror" v-if="usernameValidity === 'invalid'">{{error}} </div>
   </div>
 </template>
 
 <script>
-  export default {
-
-    props: ["name"],
-    data() {
-      return {
+export default {
+  props: ["name","error"],
+  data() {
+    return {
+      value: "",
+      values: {
+        key: "",
         value: "",
-        values:{
-          key:"",
-          value:"",
-        }
+        usernameValidity:'pending',
+      },
+    };
+  },
+  methods: {
+    handleField(e) {
+      this.values.key = e.target.name;
+      this.values.value = e.target.value;
+      this.$store.dispatch("getValueFromInputField", this.values);
+    },
+    validateInput(){
+      if(this.value=== ""){
+        this.usernameValidity='invalid';
+
+      }
+      else{
+
+        this.usernameValidity="valid";
       }
 
-    },
-     methods: {
-      handleField(e) {
-            this.values.key=e.target.name;
-            this.values.value=e.target.value;
-            this.$store.dispatch("getValueFromInputField",this.values)
-        }
+
     }
-  }
+  },
+};
 </script>
 
 
 
 <style scoped>
-
-#validationDefault02{
+.inputtext{
   width: 100%;
-    height: 50px;
-    
-    padding-left: 5px;
-    padding-right: 5px;
-    padding-top: 15px;
-    padding-bottom: 15;
-    border: 1px solid grey;
-    
-   
-    border-radius: 5px;
+  height: 50px;
+  padding: 10px 10px;
+  /* padding-left: 5px;
+  padding-right: 5px;
+  padding-top: 15px;
+  padding-bottom: 15; */
+  border: 1px solid grey;
+
+  border-radius: 5px;
+}
+.inputtexterror{
+
+color:red
+  
 }
 </style>
